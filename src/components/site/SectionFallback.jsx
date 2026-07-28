c508dd2acfcdcb17304f0c33b7f77744e2fae98f
prakashelectronics10 <prakashelectronics10@gmail.com>
2026-07-29 00:14:28 +0530

    Delete src/components directory

--- src/components/site/SectionFallback.jsx
+++
@@ -1,5 +0,0 @@
-import { LoadingState } from "./StateLottie";
-
-export function SectionFallback({ message = "Loading content..." }) {
-  return <LoadingState message={message} className="site-state-lottie--section" />;
-}
