f1d16a6d29470e64363d6f6aae3b0d7a00d3ce6e
prakashelectronics10 <prakashelectronics10@gmail.com>
2026-07-28 13:44:15 +0530

    Delete server directory

--- server/routes/invoicePublicRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { publicInvoicePdf } = require("../controllers/invoiceController");
-
-const router = express.Router();
-
-router.get("/:token/pdf", publicInvoicePdf);
-
-module.exports = router;
